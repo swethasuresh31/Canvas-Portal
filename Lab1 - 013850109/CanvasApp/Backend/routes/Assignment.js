49da64939755759b997bdbe65bc2370871eca637
Swetha Suresh <swetha.suresh@sjsu.edu>
2019-03-16 09:55:25 -0700

    assignment backend

--- Lab1 - 013850109/CanvasApp/Backend/routes/Assignment.js
+++ Lab1 - 013850109/CanvasApp/Backend/routes/Assignment.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('../db/connection')
+var path = require('path')
+
+var multer = require('multer');
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/files/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+let upload = multer({ storage });
 
 router.get('/:courseUid', function (req, res) {
     console.log("Getting assignments for course: " + req.params.courseUid)
-    connection.query('select * from coursework where course_uid = ? and coursework_type=?;', [req.params.courseUid,'assignment'], function (error, results, fields) {
+    connection.query('select * from coursework where course_uid = ? and coursework_type=?;', [req.params.courseUid, 'assignment'], function (error, results, fields) {
         if (error) {
             console.log(error)
             res.status(500).send(error);
@@ -26,28 +38,36 @@ router.get('/:courseUid/:courseworkUid', function (req, res) {
     });
 })
 
-router.post('/:courseUid/:courseworkUid', function (req, res) {
-    console.log("posting assignment for course: " + req.params.courseUid)
-    connection.query('select * from student_courses where course_uid=? and email_id=?;', [req.params.courseUid, req.body.user], function (error, results, fields) {
-        if (error) {
-            console.log(error || results.length !== 1)
-            res.status(500).send(error);
-        } else {
-            addQuizResult(results[0].studentcourses_uid, req.params.courseworkUid, req.body.user, req.body.totalPoints, res);
-        }
-    });
+router.post('/:courseUid/:courseworkUid/:user', upload.single('file'), function (req, res) {
+    console.log(req.file)
+    if (!req.file) {
+        res.status(500).send("No file");
+    } else {
+        console.log("posting assignment for course: " + req.params.courseUid)
+        connection.query('select * from student_courses where course_uid=? and email_id=?;', [req.params.courseUid, req.params.user], function (error, results, fields) {
+            if (error) {
+                console.log(error || results.length !== 1)
+                res.status(500).send(error);
+            } else {
+                console.log(results[0].studentcourses_uid)
+                addAssignmentResult(results[0].studentcourses_uid, req.params.courseworkUid, req.params.user, 0, res)
+
+            }
+        });
+    }
 })
 
-addQuizResult = (studentcoursesUid, courseworkUid, emailId, totalPoints, res) => {
+addAssignmentResult = (studentcoursesUid, courseworkUid, emailId, totalPoints, res) => {
+    console.log(studentcoursesUid + '' + courseworkUid + emailId + totalPoints)
     connection.query('insert into student_coursework(studentcourses_uid, coursework_uid, email_id, scored_points)' +
-    ' values (?,?,?,?) ON DUPLICATE KEY UPDATE scored_points=?;', [studentcoursesUid, courseworkUid, emailId, totalPoints, totalPoints], function (error, results, fields) {
-        if (error) {
-            console.log(error)
-            res.status(500).send(error);
-        } else {
-            res.status(200).send("Success")
-        }
-    });
+        ' values (?,?,?,?) ON DUPLICATE KEY UPDATE scored_points=?;', [studentcoursesUid, courseworkUid, emailId, totalPoints, totalPoints], function (error, results, fields) {
+            if (error) {
+                console.log(error)
+                res.status(500).send(error);
+            } else {
+                res.status(200).send("Success")
+            }
+        });
 }
 
 router.post('/:courseUid', function (req, res) {
