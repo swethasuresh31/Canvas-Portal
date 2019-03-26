c580238372b450761a35a4b447dbfb2f3d311cd0
Swetha Suresh <swetha.suresh@sjsu.edu>
2019-03-26 14:40:43 -0700

    account fix

--- Lab2 - 013850109/CanvasApp/Frontend/src/components/Account/Account.js
+++ Lab2 - 013850109/CanvasApp/Frontend/src/components/Account/Account.js
@@ -129,7 +129,7 @@ class Account extends Component {
       data.append('file', this.fileInput.current.files[0], filename)
       console.log(data)
       //adds the image based on information entered
-       await axios.post('http://localhost:3001/account/img/' + encodeURI(this.state.emailId), data)
+      await axios.post('http://localhost:3001/account/img/' + encodeURI(this.state.emailId), data)
         .then((response) => {
           console.log(response);
           if (response !== undefined)
@@ -141,7 +141,7 @@ class Account extends Component {
   }
 
   async saveProfile(e) {
-    await this.profileImage();
+    // await this.profileImage();
     axios.defaults.headers.common['Authorization'] = 'jwt ' + localStorage.getItem('userToken');
     const accountUpd = {
       name: this.state.name,
@@ -162,131 +162,133 @@ class Account extends Component {
     //make a put request with the user data
     await this.props.updateProfileInformation(accountUpd);
     this.setState({
-      isEditing:"false"
+      isEditing: "false"
     })
   }
 
-  async componentDidMount() {
+  async componentWillMount() {
+    console.log("started mount of account")
     await this.props.getProfileInformation();
-    const result = this.props.profileStateStore.result.data;
-    var keyData = Object.keys(this.props.profileStateStore.result.data);
+    console.log("finished mount of account")
+    console.log("Store result: " + JSON.stringify(this.props))
+    const result = this.props.profileStateStore.result;
+    if (result) {
+      var keyData = Object.keys(result.data);
 
-    for (var i = 0; i < keyData.length; i++) {
-      var name = keyData[i];
-      console.log('result[i]', result[name])
-      this.setState({
-        [name]: result[name]
-      });
+      for (var i = 0; i < keyData.length; i++) {
+        var name = keyData[i];
+        console.log('result[i]', result[name])
+        this.setState({
+          [name]: result[name]
+        });
+      }
+      console.log('state', this.state);
     }
-    console.log('state', this.state);
   }
 
 
 
   render() {
-    if (this.props.loginStateStore.result) {
-      if (this.props.loginStateStore.result.authFlag === true) {
-        if (this.state.isEditing === "true") {
-          return (
-            <div>
-              {this.state.redirectVar}
-              <div className="row">
-                <div className="col" style={{ maxWidth: "100px" }}>
-                  <Navbar selected="account" />
-                </div>
-                <div className="col">
-                  <div className="row">
-                    <div className="col">
-                      <br />
-                      <Heading theme={{ borderPadding: "1rem" }} border="bottom">{this.state.name}'s Profile
+    if (localStorage.userToken) {
+      if(!this.props.profileStateStore.result) return null;
+      if (this.state.isEditing === "true") {
+        return (
+          <div>
+            {this.state.redirectVar}
+            <div className="row">
+              <div className="col" style={{ maxWidth: "100px" }}>
+                <Navbar selected="account" />
+              </div>
+              <div className="col">
+                <div className="row">
+                  <div className="col">
+                    <br />
+                    <Heading theme={{ borderPadding: "1rem" }} border="bottom">{this.state.name}'s Profile
                   <span style={{ float: "right" }}><button type="button" class="btn btn-secondary btn mr-3" onClick={() => { this.setState({ isEditing: "false" }) }}><IconEditLine /> Cancel Edit</button>
-                        </span>
-                      </Heading>
+                      </span>
+                    </Heading>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col col-sm-2 pl-5">
+                    <br /><Avatar src={this.state.img} name={this.state.name} size="x-large" />
+                    <div className="row mt-2">
+                      <div className="col">
+                        <input type="file" name="assignmentFile" id="assignmentFile" ref={this.fileInput}></input>
+                      </div>
                     </div>
                   </div>
-                  <div className="row">
-                    <div className="col col-sm-2 pl-5">
-                      <br /><Avatar src={this.state.img} name={this.state.name} size="x-large" />
-                      <div className="row mt-2">
-                        <div className="col">
-                          <input type="file" name="assignmentFile" id="assignmentFile" ref={this.fileInput}></input>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col col-lg-5">
-                      <br /><h4>
-                        <table className="table-borderless"
-                        ><tbody>
-                            <tr><td><label>Name: </label></td><td><input type="text" class="form-control" id="name" aria-describedby="emailHelp" onChange={this.nameChangeHandler} /></td></tr>
-                            <tr><td><label>Email: </label></td><td><fieldset disabled><input type="text" class="form-control" id="emailId" aria-describedby="emailHelp" value={this.state.emailId} /></fieldset></td></tr>
-                            <tr><td><label>Phone Number: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.phoneNoChangeHandler} /></td></tr>
-                            <tr><td><label>About Me: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.aboutMeChangeHandler} /></td></tr>
-                            <tr><td><label>City: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.cityChangeHandler} /></td></tr>
-                            <tr><td><label>Country: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.countryChangeHandler} /></td></tr>
-                            <tr><td><label>Company: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.companyChangeHandler} /></td></tr>
-                            <tr><td><label>School: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.schoolChangeHandler} /></td></tr>
-                            <tr><td><label>Hometown: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.hometownChangeHandler} /></td></tr>
-                            <tr><td><label>Languages: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.languagesChangeHandler} /></td></tr>
-                            <tr><td><label>Gender: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.genderChangeHandler} /></td></tr>
-                          </tbody>
-                        </table></h4>
-                      <div align="center">
-                        <button type="button" class="btn btn-secondary btn m-2" onClick={() => { this.setState({ isEditing: "false" }) }}>Cancel</button><button type="button" class="btn btn-primary btn m-2" onClick={this.saveProfile}>Save</button>
-                      </div>
+                  <div className="col col-lg-5">
+                    <br /><h4>
+                      <table className="table-borderless"
+                      ><tbody>
+                          <tr><td><label>Name: </label></td><td><input type="text" class="form-control" id="name" aria-describedby="emailHelp" onChange={this.nameChangeHandler} /></td></tr>
+                          <tr><td><label>Email: </label></td><td><fieldset disabled><input type="text" class="form-control" id="emailId" aria-describedby="emailHelp" value={this.state.emailId} /></fieldset></td></tr>
+                          <tr><td><label>Phone Number: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.phoneNoChangeHandler} /></td></tr>
+                          <tr><td><label>About Me: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.aboutMeChangeHandler} /></td></tr>
+                          <tr><td><label>City: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.cityChangeHandler} /></td></tr>
+                          <tr><td><label>Country: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.countryChangeHandler} /></td></tr>
+                          <tr><td><label>Company: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.companyChangeHandler} /></td></tr>
+                          <tr><td><label>School: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.schoolChangeHandler} /></td></tr>
+                          <tr><td><label>Hometown: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.hometownChangeHandler} /></td></tr>
+                          <tr><td><label>Languages: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.languagesChangeHandler} /></td></tr>
+                          <tr><td><label>Gender: </label></td><td><input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" onChange={this.genderChangeHandler} /></td></tr>
+                        </tbody>
+                      </table></h4>
+                    <div align="center">
+                      <button type="button" class="btn btn-secondary btn m-2" onClick={() => { this.setState({ isEditing: "false" }) }}>Cancel</button><button type="button" class="btn btn-primary btn m-2" onClick={this.saveProfile}>Save</button>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          );
-        } else {
-          return (
-            <div>
-              {this.state.redirectVar}
-              <div className="row">
-                <div className="col" style={{ maxWidth: "100px" }}>
-                  <Navbar selected="account" />
-                </div>
-                <div className="col">
-                  <div className="row">
-                    <div className="col">
-                      <br />
-                      <Heading theme={{ borderPadding: "1rem" }} border="bottom">{this.state.name}'s Profile
-                  <span style={{ float: "right" }}><button type="button" class="btn btn-secondary btn" onClick={() => { this.setState({ isEditing: "true" }) }}><IconEditLine /> Edit Profile</button>
-                        </span>
-                      </Heading>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col col-sm-2 pl-5">
-                      <br /><Avatar src={this.state.img} name={this.state.name} size="x-large" />
-                    </div>
-                    <div className="col col-lg-5">
-                      <br /><h4>
-                        <table className="table-borderless">
-                          <tbody>
-                            <tr><td><label>Name: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.name} /></td></tr>
-                            <tr><td><label>Email: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.emailId} /></td></tr>
-                            <tr><td><label>Phone Number: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.phoneNo} /></td></tr>
-                            <tr><td><label>About Me: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.aboutMe} /></td></tr>
-                            <tr><td><label>City: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.city} /></td></tr>
-                            <tr><td><label>Country: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.country} /></td></tr>
-                            <tr><td><label>Company: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.company} /></td></tr>
-                            <tr><td><label>School: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.school} /></td></tr>
-                            <tr><td><label>Hometown: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.hometown} /></td></tr>
-                            <tr><td><label>Languages: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.languages} /></td></tr>
-                            <tr><td><label>Gender: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.gender} /></td></tr>
-                          </tbody>
-                        </table></h4>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        }
+          </div>
+        );
       } else {
-        return (<div><Redirect to="/login" /></div>);
+        return (
+          <div>
+            {this.state.redirectVar}
+            <div className="row">
+              <div className="col" style={{ maxWidth: "100px" }}>
+                <Navbar selected="account" />
+              </div>
+              <div className="col">
+                <div className="row">
+                  <div className="col">
+                    <br />
+                    <Heading theme={{ borderPadding: "1rem" }} border="bottom">{this.state.name}'s Profile
+                  <span style={{ float: "right" }}><button type="button" class="btn btn-secondary btn" onClick={() => { this.setState({ isEditing: "true" }) }}><IconEditLine /> Edit Profile</button>
+                      </span>
+                    </Heading>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col col-sm-2 pl-5">
+                    <br /><Avatar src={this.state.img} name={this.state.name} size="x-large" />
+                  </div>
+                  <div className="col col-lg-5">
+                    <br /><h4>
+                      <table className="table-borderless">
+                        <tbody>
+                          <tr><td><label>Name: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.name} /></td></tr>
+                          <tr><td><label>Email: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.emailId} /></td></tr>
+                          <tr><td><label>Phone Number: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.phoneNo} /></td></tr>
+                          <tr><td><label>About Me: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.aboutMe} /></td></tr>
+                          <tr><td><label>City: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.city} /></td></tr>
+                          <tr><td><label>Country: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.country} /></td></tr>
+                          <tr><td><label>Company: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.company} /></td></tr>
+                          <tr><td><label>School: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.school} /></td></tr>
+                          <tr><td><label>Hometown: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.hometown} /></td></tr>
+                          <tr><td><label>Languages: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.languages} /></td></tr>
+                          <tr><td><label>Gender: </label></td><td><input type="text" readonly class="form-control-plaintext" id="inputName" aria-describedby="emailHelp" value={this.state.gender} /></td></tr>
+                        </tbody>
+                      </table></h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
       }
     } else {
       return (<div><Redirect to="/login" /></div>);
@@ -294,10 +296,13 @@ class Account extends Component {
   }
 }
 
-const mapStateToProps = state => ({
-  profileStateStore: state.profile,
-  loginStateStore: state.login
-})
+const mapStateToProps = state => {
+  console.log(JSON.stringify(state))
+  return {
+    profileStateStore: state.profile,
+    loginStateStore: state.login
+  }
+}
 
 //export default Profile;
 export default connect(mapStateToProps, { getProfileInformation, updateProfileInformation })(Account);
